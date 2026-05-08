dcb9c002ae75a8c9b5e2d90cd973989061dd6e84
omkardhara <omkar.dhara@gmail.com>
2026-05-08 09:22:17 +0530

    Update Work.jsx

--- src/components/Work/Work.jsx
+++ src/components/Work/Work.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { F } from "../../lib/typography";
 import { useTilt } from "../../hooks/useTilt";
 import { ODLabel } from "../shared/ODLabel";
